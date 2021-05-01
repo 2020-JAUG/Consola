e55f89e6813995a8699942376aed81a0a9ea40b2
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-01 17:20:36 +0200

    We created a function to show an alert

--- js/main.js
+++ js/main.js
@@ -7,20 +7,47 @@ function onYouTubeIframeAPIReady() {
         width: '527.5',
         videoId: 'XL0MRDkLq6k',
         playerVars: {
-            controls: 0
+            controls: 0,
+            showinfo: 0,
+            start: 2,
+
+            events: {
+                'onReady': onPlayerReady,
+            }
         }
     });
 
-        let button = document.getElementById("on");
+        let button = document.getElementById('on');
+        let volumen = document.getElementById('vol');
 
+        volumen.addEventListener('click',volUp);
         button.addEventListener('click', play, false);
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    player.pauseVideo();
+
+
+    event.target.setVolume(100);
+    player.setVolume();
+
 }
 
 function play() {
 
-    if (player.getPlayerState() == YT.PlayerState.PLAYING) {
-        player.stopVideo();
+    if(player.getPlayerState() == YT.PlayerState.PLAYING) {
+        player.pauseVideo();
     } else {
         player.playVideo();
     }
 }
+
+function volUp() {
+    if (player.setVolume() < 50) {
+      player.setVolume() = player.setVolume ++, 20;
+      sound.volume = currentVolume / 50;
+    } else {
+      alert("Max Vol")
+    }
+  }
