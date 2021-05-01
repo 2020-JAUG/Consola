3444ec937934a738f52476c498ce129139d9a5c6
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-01 09:59:57 +0200

    We add event on click and we test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const of = document.querySelector('#of');
+const on = document.querySelector('#on');
+
+of.addEventListener('click', () => {
+    alert("desde btn-home");
+});
+
+on.addEventListener('click', () => {
+   alert('test2');
+});
